5f7ae278edc39df59f7e3b0d67de68f0a1cd9dc7
Adan-Asim <adan.asim@educative.io>
2024-05-13 00:42:18 +0500

    Nit: Added loading state

--- frontend/app/components/FilesSection.jsx
+++ frontend/app/components/FilesSection.jsx
@@ -12,6 +12,7 @@ const FilesSection = ({
     <h3 className="text-xl font-bold text-cyan-500 mb-4">Previous Files</h3>
     <ul className="text-gray-400 overflow-auto h-auto mb-4">
       {loading && <p>Loading Data...</p>}
+      {uploading && <p>Uploading File...</p>}
       {!loading &&
         files.map((file) => (
           <li
@@ -27,7 +28,7 @@ const FilesSection = ({
           </li>
         ))}
     </ul>
-    <label className="bg-cyan-500 hover:bg-cyan-600 justify-start text-white font-bold py-1 px-6 rounded focus:outline-none focus:shadow-outline transition-colors duration-200">
+    <label className="bg-cyan-500 hover:bg-cyan-600 justify-start text-white font-bold py-1 px-6 rounded focus:outline-none focus:shadow-outline transition-colors duration-200 cursor-pointer">
       + Upload New File
       <input
         type="file"
